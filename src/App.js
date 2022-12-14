aa3c42bdcde360a6229fd358e584ea9f2b51e942
Anuj Kaushik <anujkaushik1512@gmail.com>
2022-12-14 12:22:55 +0530

    Implemented Navbar & Card Items

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import CardItems from './Components/CardItems';
+
+function MemoriesNavbar() {
+  return (
+    <div className='memories'>
+      <span>Memories</span>
+    </div>
+
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='navbar'>
+        <MemoriesNavbar/>
+      </div>
+
+      <CardItems/>
+    </>
+      
+      
   );
 }
 
---
+++ src/Components/CardItems.js
@@ -0,0 +1,101 @@
+import React from 'react'
+import './CardItems.css'
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { Grid, Paper } from '@mui/material';
+
+function CardItems() {
+  return (
+    <div className='carditems-main'>
+
+      <div className='cards'>
+
+        <Grid md = {10} container position={'relative'} left={'15.5rem'} top = {'10px'}>
+          <Grid item paddingLeft={2} paddingRight = {2} paddingTop = {2}>
+            <Card variant='elevation' sx={{ width : 250}}>
+              <CardMedia
+                component="img"
+                height="140"
+                image="/static/images/cards/contemplative-reptile.jpg"
+                alt="green iguana"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Lizard
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Lizards are a widespread group of squamate reptiles, with over 6,000
+                  species, ranging across all continents except Antarctica
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small">Share</Button>
+                <Button size="small">Learn More</Button>
+              </CardActions>
+            </Card>
+          </Grid>
+
+          <Grid item paddingLeft={2} paddingRight = {2} paddingTop = {2}>
+            <Card sx={{ width : 250}}>
+              <CardMedia
+                component="img"
+                height="140"
+                image="/static/images/cards/contemplative-reptile.jpg"
+                alt="green iguana"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Lizard
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Lizards are a widespread group of squamate reptiles, with over 6,000
+                  species, ranging across all continents except Antarctica
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small">Share</Button>
+                <Button size="small">Learn More</Button>
+              </CardActions>
+            </Card>
+          </Grid>
+
+          <Grid item paddingLeft={2} paddingRight = {2} paddingTop = {2}>
+            <Card sx={{ width : 250}}>
+              <CardMedia
+                component="img"
+                height="140"
+                image="/static/images/cards/contemplative-reptile.jpg"
+                alt="green iguana"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Lizard
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Lizards are a widespread group of squamate reptiles, with over 6,000
+                  species, ranging across all continents except Antarctica
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small">Share</Button>
+                <Button size="small">Learn More</Button>
+              </CardActions>
+            </Card>
+          </Grid>
+
+          
+
+        </Grid>
+
+
+      </div>
+
+    </div>
+  )
+}
+
+export default CardItems
