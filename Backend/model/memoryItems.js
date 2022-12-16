3f6985779e4a6750e1266108f1123f29dbf89e68
Anuj Kaushik <anujkaushik1512@gmail.com>
2022-12-16 18:35:19 +0530

    Created Schema & Model

---
+++ Backend/model/memoryItems.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const memorySchema = new mongoose.Schema({
+    creator : {
+        type : String,
+        required : [true, 'Please add creator'],
+        maxlength : [50, 'Creator cannot be more than 50 char']
+    },
+    title : {
+        type : String,
+        required : [true, 'Please add title'],
+        maxlength : [50, 'Title cannot be more than 50 char']
+    },
+    message : {
+        type : String,
+        requried : [true, 'Please add message'],
+        maxlength : [500, 'Message cannot be more than 500 char']
+    },
+
+    tags : {
+        type : String,
+        requried : [true, 'Please add tags'],
+        maxlength : [50, 'Tags cannot be more than 50 char']
+    },
+
+    file : {
+        type : String,
+        
+    }
+})
